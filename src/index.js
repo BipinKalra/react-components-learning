f238747236e13a06dba0841f3bd57c5fd724af49
BipinKalra <bipinkalra@gmail.com>
2021-03-02 14:13:25 +0530

    basic comments component

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img
+            alt="avatar"
+            src="https://source.unsplash.com/random"
+            style={{ height: "35px", width: "35px" }}
+          />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today ay 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img
+            alt="avatar"
+            src="https://source.unsplash.com/random"
+            style={{ height: "35px", width: "35px" }}
+          />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today ay 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img
+            alt="avatar"
+            src="https://source.unsplash.com/random"
+            style={{ height: "35px", width: "35px" }}
+          />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today ay 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
