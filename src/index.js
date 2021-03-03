93d125a6b9c28a641f8c3c864e150156804b0a1b
BipinKalra <bipinkalra@gmail.com>
2021-03-03 13:49:45 +0530

    comments section nested

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetails from "./CommentDetails";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img
-            alt="avatar"
-            src="https://source.unsplash.com/random"
-            style={{ height: "35px", width: "35px" }}
-          />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today ay 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetails />
     </div>
   );
 };
